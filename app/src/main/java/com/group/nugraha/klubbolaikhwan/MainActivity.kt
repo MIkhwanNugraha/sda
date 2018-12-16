575d144fb0a1b8442763b709b68c4372d67c16f3
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-12-16 23:02:58 +0400

    Commit first

---
+++ app/src/main/java/com/group/nugraha/klubbolaikhwan/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.group.nugraha.klubbolaikhwan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
