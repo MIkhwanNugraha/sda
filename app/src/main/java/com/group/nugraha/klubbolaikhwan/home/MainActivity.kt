e5f619d80786513633755217fb909bf2c4f77db5
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-12-17 22:49:20 +0400

    versi kedua

--- app/src/main/java/com/group/nugraha/klubbolaikhwan/home/MainActivity.kt
+++ app/src/main/java/com/group/nugraha/klubbolaikhwan/home/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.group.nugraha.klubbolaikhwan
+package com.group.nugraha.klubbolaikhwan.home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/group/nugraha/klubbolaikhwan/model/Timbola.kt
@@ -0,0 +1,5 @@
+package com.group.nugraha.klubbolaikhwan.model
+
+data class Timbola (
+    val namaklub: String?, val logoklub: Int?
+)
