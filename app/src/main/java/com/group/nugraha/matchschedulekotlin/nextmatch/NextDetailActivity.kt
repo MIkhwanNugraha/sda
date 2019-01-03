eea7c8e2ad83aacd62e8cd1133b342bfeb7ae037
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2019-01-03 16:58:58 +0400

    3

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/nextmatch/NextDetailActivity.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/nextmatch/NextDetailActivity.kt
@@ -1,4 +1,42 @@
 package com.group.nugraha.matchschedulekotlin.nextmatch
 
-class NextDetailActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ProgressBar
+import com.group.nugraha.matchschedulekotlin.model.EventsItem
+
+class NextDetailActivity : AppCompatActivity(), NextView {
+    var idEvent: String = ""
+    var idTandang: String = ""
+    var idKandang: String = ""
+    var nameKandang: String = ""
+    var nameTandang: String = ""
+
+    private lateinit var progresBar: ProgressBar
+    private lateinit var presenter: NextDetailPresenter
+    private lateinit var events: EventsItem
+
+    companion object {
+        const val ID_EVENTS = "id_events"
+        const val ID_TANDANG = "id_Tandang"
+        const val ID_KANDANG = "id_Kandang"
+        const val KANDANG_NAME = "kandang_name"
+        const val TANDANG_NAME = "tandang_name"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.layoutnextdetail)
+
+    override fun showLoading() {
+
+    }
+
+    override fun hideLOading() {
+        }
+
+    override fun showEventList(data: List<EventsItem>) {
+  }
+
 }
