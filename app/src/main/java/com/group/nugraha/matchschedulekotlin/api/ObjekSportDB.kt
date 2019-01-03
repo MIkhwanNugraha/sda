47ade6453729aeb6ddad210efa1851502ff17b78
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2019-01-03 16:45:39 +0400

    3

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/api/ObjekSportDB.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/api/ObjekSportDB.kt
@@ -6,4 +6,8 @@ object ObjekSportDB {
     fun getEventNext(league: String?): String{
         return "${BuildConfig.BASE_URL}api/v1/json/${BuildConfig.TSDB_API_KEY}/eventsnextleague.php?id=" + league
     }
+
+    fun getLookUpNDP(eventId: String?): String{
+        return "${BuildConfig.BASE_URL}api/v1/json/${BuildConfig.TSDB_API_KEY}/lookupevent.php?id=" + eventId
+    }
 }
