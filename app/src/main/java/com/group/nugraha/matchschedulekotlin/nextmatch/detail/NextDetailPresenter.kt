b7b5c70a9721b16ad8e5f7e2ef0ed33ff31857e2
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2019-01-07 20:35:50 +0400

    1

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/nextmatch/detail/NextDetailPresenter.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/nextmatch/detail/NextDetailPresenter.kt
@@ -5,39 +5,36 @@ import com.group.nugraha.matchschedulekotlin.api.ApiRepository
 import com.group.nugraha.matchschedulekotlin.api.ObjekSportDB
 import com.group.nugraha.matchschedulekotlin.model.EventsResponse
 import com.group.nugraha.matchschedulekotlin.model.TeamResponse
-import com.group.nugraha.matchschedulekotlin.util.CoroutineContextProvider
 import kotlinx.coroutines.*
-
-
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 
 class NextDetailPresenter (private val view: NextDetailView,
                            private val apiRepository: ApiRepository,
-                           private val gson: Gson,
-                           private val context: CoroutineContextProvider = CoroutineContextProvider()){
+                           private val gson: Gson){
 
     fun getEventDetail(eventId: String?, homeTeamId: String?, awayTeamId: String?){
         view.tunjukkanLoading()
 
-        GlobalScope.launch(context.main) {
-                val matchDetail = gson.fromJson(apiRepository
+        doAsync {
+            val matchDetail = gson.fromJson(apiRepository
                     .doRequest(ObjekSportDB.getLookUpNDP(eventId)),
                     EventsResponse::class.java)
 
 
-                val homeTeam = gson.fromJson(ApiRepository()
+            val homeTeam = gson.fromJson(ApiRepository()
                     .doRequest(ObjekSportDB.getTeamDetail(homeTeamId)),
                     TeamResponse::class.java)
 
-                val awayTeam =  gson.fromJson(ApiRepository()
+            val awayTeam =  gson.fromJson(ApiRepository()
                     .doRequest(ObjekSportDB.getTeamDetail(awayTeamId)),
                     TeamResponse::class.java)
 
-
+            uiThread {
                 view.showDetail(matchDetail.Events, homeTeam.teams, awayTeam.teams)
                 view.sembunyikanLoading()
-
-
+            }
 
         }
     }
