6607a84efb10e9e57ac082b8466c3ab594f9eae3
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2019-01-12 23:10:23 +0400

    33

--- app/src/main/java/com/group/nugraha/matchschedulekotlin/preferred/PreferredActivity.kt
+++ app/src/main/java/com/group/nugraha/matchschedulekotlin/preferred/PreferredActivity.kt
@@ -1,6 +1,48 @@
 package com.group.nugraha.matchschedulekotlin.preferred
 
+import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.ProgressBar
+import com.group.nugraha.matchschedulekotlin.R
+import com.group.nugraha.matchschedulekotlin.db.Preferred
+import com.group.nugraha.matchschedulekotlin.db.database
+import org.jetbrains.anko.support.v4.onRefresh
+import java.util.ArrayList
 
 class PreferredActivity : AppCompatActivity() {
+    private var preferred: MutableList<Preferred> = mutableListOf()
+    private lateinit var adapter: PreferredAdapter
+    private lateinit var rvPreferred: RecyclerView
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var progressBar: ProgressBar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layoutpreferred)
+
+        progressBar = findViewById(R.id.pgBarPreferred)
+        swipeRefresh =findViewById(R.id.swipePreferred)
+        rvPreferred = findViewById(R.id.rvPreferred)
+        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 1)
+        rvPreferred.layoutManager = layoutManager
+
+        adapter = PreferredAdapter(this, preferred as ArrayList<Preferred>)
+        rvPreferred.adapter = adapter
+        showPreferred()
+
+        swipeRefresh.onRefresh{
+            preferred.clear()
+            showPreferred()
+
+        }
+    }
+
+    private fun showPreferred(){
+        this?.database?.use{
+
+        }
+    }
 }
